8a20c5313e5581834719fdbd4f92cf8b65a6eea5
rohit <rohitsalem@gmail.com>
2017-08-15 11:27:19 -0700

    Added ROS node for object detection

---
+++ scripts/detect_ros.py
@@ -0,0 +1,168 @@
+#!usr/bin/python
+## Author: Rohit
+## Date: July, 25, 2017 
+#Purpose: Ros node to detect objects using tensorflow 
+
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import cv2
+import object_detection
+from collections import defaultdict
+from io import StringIO
+import matplotlib
+from matplotlib import pyplot as plt
+
+#for ros:
+import rospy
+from std_msgs.msg import String 
+from std_msgs.msg import Header
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError 
+from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
+from copy import deepcopy
+
+#for time
+import time
+
+# This is needed utils is stored in object_detection folder.
+sys.path.append('object_detection')
+
+# ## Object detection imports
+# Here are the imports from the object detection module.
+from utils import label_map_util
+
+from utils import visualization_utils as vis_util
+
+# What model to use
+MODEL_NAME =  'ssd_mobilenet_v1_coco_11_06_2017'
+MODEL_PATH = '../data/models/' + MODEL_NAME
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = MODEL_PATH + '/frozen_inference_graph.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = os.path.join('../data/labels', 'mscoco_label_map.pbtxt')
+
+NUM_CLASSES = 90
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+# ## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# # Detection
+with detection_graph.as_default():
+  with tf.Session(graph=detection_graph) as sess:
+    class detector: 
+
+      def __init__(self):
+        self.image_pub = rospy.Publisher("/detector/image_raw",Image, queue_size=1)
+        self.object_pub = rospy.Publisher("/detector/Objects", Detection2DArray, queue_size=1)
+        self.detect_pub = rospy.Publisher("/detector/Object", Detection2D, queue_size=1)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw" , Image , self.callback, queue_size=1, buff_size=2**24)
+        
+
+      def callback(self,data):
+       
+        objArray = Detection2DArray()
+        try:
+          cv_image = self.bridge.imgmsg_to_cv2(data,"bgr8")
+        except CvBridgeError as e:
+          print(e)
+        image=cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
+        # print(image.size)
+        # the array based representation of the image will be used later in order to prepare the
+        # result image with boxes and labels on it.
+        image_np = np.asarray(image)
+        # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        # Each box represents a part of the image where a particular object was detected.
+        boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        # Each score represent how level of confidence for each of the objects.
+        # Score is shown on the result image, together with the class label.
+        scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        (boxes, scores, classes, num_detections) = sess.run([boxes, scores, classes, num_detections],
+            feed_dict={image_tensor: image_np_expanded})
+        objects=vis_util.visualize_boxes_and_labels_on_image_array(
+            image,
+            np.squeeze(boxes),
+            np.squeeze(classes).astype(np.int32),
+            np.squeeze(scores),
+            category_index,
+            use_normalized_coordinates=True,
+            line_thickness=2)
+
+        objArray.detections =[]
+        objArray.header=data.header
+        # print len(objects)
+        object_count=1
+
+        for i in range(len(objects)):
+          # print ("Object Number: %d "  %object_count)
+          object_count+=1  
+          objArray.detections.append(self.object_predict(objects[i],data.header,image_np,cv_image))
+          
+        self.object_pub.publish(objArray) 
+        
+        img=cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)      
+        image_out = Image()
+        try:
+          image_out = self.bridge.cv2_to_imgmsg(img,"bgr8")
+        except CvBridgeError as e:
+          print(e)
+        image_out.header = data.header
+        self.image_pub.publish(image_out)
+      
+      def object_predict(self,object_data, header, image_np,image):
+        
+        obj=Detection2D() 
+        obj_hypothesis= ObjectHypothesisWithPose()  
+        
+        object_id=object_data[0]
+        object_score=object_data[1]
+        dimensions=object_data[2]
+        # image_width,image_height=image.size
+        # print (image_width)
+        # print (image_height)
+        # print(image.size)
+        obj.header=header
+        obj_hypothesis.id = object_id
+        obj_hypothesis.score = object_score
+        obj.results.append(obj_hypothesis)
+        obj.bbox.size_y = int((dimensions[2]-dimensions[0])*480)
+        obj.bbox.size_x = int((dimensions[3]-dimensions[1] )*640)
+        obj.bbox.center.y = int((dimensions[1] + dimensions [3])*480/2)
+        obj.bbox.center.x = int((dimensions[0] + dimensions[2])*640/2)
+
+        return obj 
+
+def main(args):
+  obj=detector()
+  rospy.init_node('detector_node')
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("ShutDown")
+  cv2.destroyAllWindows()
+
+if __name__=='__main__':
+  main(sys.argv)
