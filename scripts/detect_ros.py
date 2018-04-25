f303c5ad883875853ae27ba76ba9bc36fd524587
rohit <rohitsalem@gmail.com>
2018-04-24 19:33:09 -0700

    Code cleanup

--- scripts/detect_ros.py
+++ scripts/detect_ros.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 ## Author: Rohit
-## Date: July, 25, 2017 
-#Purpose: Ros node to detect objects using tensorflow 
+## Date: July, 25, 2017
+# Purpose: Ros node to detect objects using tensorflow
 
-import numpy as np
 import os
-import six.moves.urllib as urllib
 import sys
-import tarfile
+import cv2
+import numpy as np
 try:
     import tensorflow as tf
 except ImportError:
@@ -15,52 +14,33 @@ except ImportError:
     print("  sudo apt install python-pip")
     print("  sudo pip install tensorflow")
     sys.exit(1)
-import zipfile
-import cv2
-import object_detection
-from collections import defaultdict
-from io import StringIO
-import matplotlib
-from matplotlib import pyplot as plt
 
-#for ros:
+# ROS related imports
 import rospy
-from std_msgs.msg import String 
-from std_msgs.msg import Header
+from std_msgs.msg import String , Header
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError 
+from cv_bridge import CvBridge, CvBridgeError
 from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
-from copy import deepcopy
 
-#for time
-import time
-
-# This is needed utils is stored in object_detection folder.
-# sys.path.append('object_detection')
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
+# Object detection module imports
+import object_detection
 from object_detection.utils import label_map_util
-
 from object_detection.utils import visualization_utils as vis_util
 
 # SET FRACTION OF GPU YOU WANT TO USE HERE
-GPU_FRACTION = 0.7
+GPU_FRACTION = 0.4
 
-# What model to use
-######### CHANGE THE MODEL NAME HERE ############
+######### Set model here ############
 MODEL_NAME =  'ssd_mobilenet_v1_coco_11_06_2017'
-# By default models are stored in data/models/ 
+# By default models are stored in data/models/
 MODEL_PATH = os.path.join(os.path.dirname(sys.path[0]),'data','models' , MODEL_NAME)
-
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_PATH + '/frozen_inference_graph.pb'
-######### CHANGE THE LABEL NAME HERE ###########
+######### Set the label map file here ###########
 LABEL_NAME = 'mscoco_label_map.pbtxt'
-# List of the strings that is used to add correct label for each box.
+# By default label maps are stored in data/labels/
 PATH_TO_LABELS = os.path.join(os.path.dirname(sys.path[0]),'data','labels', LABEL_NAME)
-
-###CHANGE NUMBER OF CLASSES HERE ####
+######### Set the number of classes here #########
 NUM_CLASSES = 90
 
 detection_graph = tf.Graph()
@@ -71,27 +51,29 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
+## Loading label map
+# Label maps map indices to category names, so that when our convolution network predicts `5`,
+# we know that this corresponds to `airplane`.  Here we use internal utility functions,
+# but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
+# Setting the GPU options to use fraction of gpu that has been set
 config = tf.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = GPU_FRACTION
 
-# # Detection
+# Detection
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph,config=config) as sess:
-    class detector: 
+    class detector:
 
       def __init__(self):
         self.image_pub = rospy.Publisher("debug_image",Image, queue_size=1)
         self.object_pub = rospy.Publisher("objects", Detection2DArray, queue_size=1)
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("image", Image, self.image_cb, queue_size=1, buff_size=2**24)
-        
+
       def image_cb(self, data):
         objArray = Detection2DArray()
         try:
@@ -99,7 +81,7 @@ with detection_graph.as_default():
         except CvBridgeError as e:
           print(e)
         image=cv2.cvtColor(cv_image,cv2.COLOR_BGR2RGB)
-        
+
         # the array based representation of the image will be used later in order to prepare the
         # result image with boxes and labels on it.
         image_np = np.asarray(image)
@@ -126,17 +108,15 @@ with detection_graph.as_default():
 
         objArray.detections =[]
         objArray.header=data.header
-        # print len(objects)
         object_count=1
 
         for i in range(len(objects)):
-          # print ("Object Number: %d "  %object_count)
-          object_count+=1  
+          object_count+=1
           objArray.detections.append(self.object_predict(objects[i],data.header,image_np,cv_image))
-          
-        self.object_pub.publish(objArray) 
-        
-        img=cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)      
+
+        self.object_pub.publish(objArray)
+
+        img=cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
         image_out = Image()
         try:
           image_out = self.bridge.cv2_to_imgmsg(img,"bgr8")
@@ -144,16 +124,16 @@ with detection_graph.as_default():
           print(e)
         image_out.header = data.header
         self.image_pub.publish(image_out)
-      
+
       def object_predict(self,object_data, header, image_np,image):
         image_height,image_width,channels = image.shape
-        obj=Detection2D() 
-        obj_hypothesis= ObjectHypothesisWithPose()  
-        
+        obj=Detection2D()
+        obj_hypothesis= ObjectHypothesisWithPose()
+
         object_id=object_data[0]
         object_score=object_data[1]
         dimensions=object_data[2]
-       
+
         obj.header=header
         obj_hypothesis.id = object_id
         obj_hypothesis.score = object_score
@@ -163,13 +143,11 @@ with detection_graph.as_default():
         obj.bbox.center.x = int((dimensions[1] + dimensions [3])*image_height/2)
         obj.bbox.center.y = int((dimensions[0] + dimensions[2])*image_width/2)
 
-        return obj 
+        return obj
 
 def main(args):
-  
   rospy.init_node('detector_node')
   obj=detector()
-
   try:
     rospy.spin()
   except KeyboardInterrupt:
